271562da16d0c794ce0f949d5a5b9a4fa2beba0e
Divish Ram <64987750+divishram@users.noreply.github.com>
2022-03-12 22:32:52 -0500

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -12,33 +12,33 @@ include( 'includes/header.php' );
 
 <ul id="dashboard">
   <li>
-    <a href="projects.php">
-      Manage Projects
-      <img src="icons/projects.svg" alt="Logout icon" height=35/>
+    <a href="projects.php" style="justify-content:center;">
+      <span style="align-self:center;">Manage Projects</span>
+      <img style="align-self:center;" src="icons/projects.svg" alt="Projects icon" height=35/>
     </a>
   </li>
   <li>
-    <a href="skills.php">
-      Manage Skills
-      <img src="icons/skills.svg" alt="Logout icon" height=35/>
+    <a href="skills.php" style="justify-content:center;">
+      <span style="align-self:center;">Manage Skills</span>
+      <img style="align-self:center;" src="icons/skills.svg" alt="Skills icon" height=35/>
     </a>
   </li>
   <li>
-    <a href="contact_requests.php">
-      Contact Requests
-      <img src="icons/contact.svg" alt="Logout icon" height=35/>
+    <a href="contact_requests.php" style="justify-content:center;">
+      <span style="align-self:center;">Contact Requests</span>
+      <img style="align-self:center;" src="icons/contact.svg" alt="Contact icon" height=35/>
     </a>
   </li>
   <li>
-    <a href="users.php">
-      Manage Users
-      <img src="icons/users.svg" alt="Logout icon" height=35/>
+    <a href="users.php" style="justify-content:center;">
+      <span style="align-self:center;">Manage Users</span>
+      <img style="align-self:center;" src="icons/users.svg" alt="Users icon" height=35/>
     </a>
   </li>
   <li>
-    <a href="logout.php">
-      Logout
-      <img src="icons/logout.svg" alt="Logout icon" height=35/>
+    <a href="logout.php" style="justify-content:center;">
+      <span style="align-self:center;">Logout</span>
+      <img style="align-self:center;" src="icons/logout.svg" alt="Logout icon" height=35/>
     </a>
   </li>
 </ul>
