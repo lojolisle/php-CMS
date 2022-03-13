7ef682a03ba8bf8a649451028208ab9da831086b
Divish Ram <64987750+divishram@users.noreply.github.com>
2022-03-12 22:06:04 -0500

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -14,11 +14,13 @@ include( 'includes/header.php' );
   <li>
     <a href="projects.php">
       Manage Projects
+      <img src="icons/projects.svg" alt="Logout icon" height=35/>
     </a>
   </li>
   <li>
     <a href="skills.php">
       Manage Skills
+      <img src="icons/skills.svg" alt="Logout icon" height=35/>
     </a>
   </li>
   <li>
@@ -29,11 +31,13 @@ include( 'includes/header.php' );
   <li>
     <a href="users.php">
       Manage Users
+      <img src="icons/users.svg" alt="Logout icon" height=35/>
     </a>
   </li>
   <li>
     <a href="logout.php">
       Logout
+      <img src="icons/logout.svg" alt="Logout icon" height=35/>
     </a>
   </li>
 </ul>
